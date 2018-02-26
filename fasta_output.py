82602685f801106bf16ac7a946b4652d3a80998a
u2365 <linnea.axelsson@stud.ki.se>
2018-02-26 13:59:21 +0100

    fasta output

---
+++ fasta_output.py
@@ -0,0 +1,18 @@
+#Creates fasta file for PSI-Blast run, sequence name and sequence
+
+import pandas as pan
+
+filehandle = 'alpha_beta_globular_sp_4state.txt'
+
+raw_data = pan.read_csv(filehandle, header=None)
+print(raw_data)
+drop_list = []
+
+for a in range(len(raw_data)):
+    if (a-1) %3 == 0:
+        drop_list.extend([a])
+
+raw_data.drop(raw_data.index[drop_list], inplace=True)
+raw_data.reset_index(drop = True, inplace = True)
+
+raw_data.to_csv(filehandle+'.fasta', header=None, index=None)
