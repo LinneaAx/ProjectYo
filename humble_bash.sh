1c569fc76c08ae0fec5f1d962211a7fdf66368f2
u2365 <linnea.axelsson@stud.ki.se>
2018-03-05 18:17:58 +0100

    still making it modular, humble start to sh script for training for different windowsizes

---
+++ humble_bash.sh
@@ -0,0 +1,11 @@
+#bash script for parsing, training and optimizing for different window sizes 
+
+Take in input 3 line file sys arg1 
+
+if __name__ = '__main__':
+
+input = sys.argv[1]
+
+if len(sys.argv) > 2:
+    kernel = sys.argv[2]
+    somethingelse = sys.argv[3]
--- train_my_model.py
+++ train_my_model.py
@@ -1,7 +1,10 @@
+import parse_code
 import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
-
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 
 #takes a np.savez file and uses it as input. Will add option to change the parameters of the SVM, generate several models, generate output model file names that correspond to the parameters and model type.  
 def train_model(input_file): 
@@ -10,9 +13,21 @@ def train_model(input_file):
     y = filz['y']
 
     clf = LinearSVC()
-    clf.fit(x, y)
+    #clf.fit(x, y)
+    
+    cross_score = cross_val_score(clf, x, y, cv = 10, verbose=True)
+    #clf.get_params() #prints out the parameters the model builds on, useful later?    
+    cross_score_average = np.average(cross_score)
+    
+    
+    print(cross_score)
+    file_handle = open('val_output.txt', 'w')
+    file_handle.write(input_file, cross_score_average) # why doesnt this work??
     
     joblib.dump(clf,input_file+'.model.pkl')
     
 if __name__ == '__main__':
     train_model('output.txt.npz')
+    
+
+
