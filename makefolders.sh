425e532e4296e5207dfa0f0c15739de69e7c08c5
u2365 <linnea.axelsson@stud.ki.se>
2018-03-07 21:23:11 +0100

    moved files

--- makefolders.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#Shell script that opens a "linneas_project_(and some addition name given as an argument)" 
-#directory with the subdirectories scripts, bash, input, output, logs
-
-#mkdir /home/users/u2365/Desktop/projects I already have this folder
-mkdir -p {/home/users/u2365/Desktop/projects/linneas_project_"$@1"}/{scripts, bash, input, output, logs}
-
