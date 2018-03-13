7decd0e8a4091e2a5ebafe785db98c60922b082e
u2365 <linnea.axelsson@stud.ki.se>
2018-03-13 13:57:00 +0100

    senaste

--- scripts/randomforest.py
+++ scripts/randomforest.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -20,21 +21,6 @@ def vectors_savez(input_file):
 x, y = vectors_savez('../splitoutput/ws_35_alpha_beta_globular_sp_4state.txt_split_output.txt.npz')
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.30)
 
-#Training and testing the different classifyers LinearSVC, decisiontree and randomforest
-#SVM classifier
- 
-clf = LinearSVC()
-svm_cross_score = cross_val_score(clf, x, y, cv = 10, verbose=True)
-print('SVM cross validation done...')
-svm_cross_mean = svm_cross_score.mean()
-clf.fit(x_train, y_train)
-print('SVM training done...')
-svm_y_predicted = clf.predict(x_test)
-print(svm_y_predicted)
-svm_classreport = classification_report(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
-svm_confusionm = confusion_matrix(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
-
-
 #decision tree classifier
 
 clf = DecisionTreeClassifier(max_depth=None, min_samples_split=2, random_state=0)
@@ -45,8 +31,8 @@ clf.fit(x_train, y_train)
 print('Decision tree training done...')
 tree_y_predicted = clf.predict(x_test)
 tree_classreport = classification_report(y_test, tree_y_predicted, labels = [1, 2, 3, 4])
-tree_confusionm = confusion_matrix(y_test, tree_y_predicted, llabels = [1, 2, 3, 4])
-
+tree_confusionm = confusion_matrix(y_test, tree_y_predicted, labels = [1, 2, 3, 4])
+tree_mcc = matthews_corrcoef(y_test, tree_y_predicted)
 
 #randomforestclassifier
 
@@ -60,12 +46,30 @@ print('Random forest training done...')
 random_y_predicted = clf.predict(x_test)
 random_classreport = classification_report(y_test, random_y_predicted, labels = [1, 2, 3, 4])
 random_confusionm = confusion_matrix(y_test, random_y_predicted, labels = [1, 2, 3, 4])
+random_mcc = matthews_corrcoef(y_test, random_y_predicted)
 
+#Training and testing the different classifiers LinearSVC, decisiontree and randomforest
+#SVM classifier
+ 
+clf = LinearSVC()
+svm_cross_score = cross_val_score(clf, x, y, cv = 10, verbose=True)
+print('SVM cross validation done...')
+svm_cross_mean = svm_cross_score.mean()
+clf.fit(x_train, y_train)
+print('SVM training done...')
+svm_y_predicted = clf.predict(x_test)
+print(svm_y_predicted)
+svm_classreport = classification_report(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
+svm_confusionm = confusion_matrix(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
+svm_mcc = matthews_corrcoef(y_test, svm_y_predicted)
 
 with open ('../outputfiles/results_svm_tree_random.txt', 'w') as f:
     f.write('Cross-validation scores for LinearSVC: ' + str(svm_cross_mean)+ '\n')
     f.write('Cross-validation scores for DecisionTreeClassifier: '+ str(tree_score_mean)+ '\n')
     f.write('Cross-validation scores for RandomForestClassifier: '+ str(random_score_mean)+ '\n')
+    f.write('Matthews correlation coefficient (MCC) SVM: ' + str(svm_mcc) + '\n')
+    f.write('Matthews correlation coefficient (MCC) DecisionTreeClassifier: ' + str(tree_mcc) + '\n')
+    f.write('Matthews correlation coefficient (MCC) RandomForestClassifier: ' + str(random_mcc) + '\n')            
     f.write('Classification report SVM: ' + '\n' + str(svm_classreport) + '\n')
     f.write('Confusion matrix SVM: ' + '\n' + str(svm_confusionm) + '\n')
     f.write('Classification report DecisionTreeClassifier: ' + '\n' + str(tree_classreport) + '\n')
