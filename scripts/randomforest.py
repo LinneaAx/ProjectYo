37d18ae6eeb7e6b2cb5d7efbaa3af7973481467d
u2365 <linnea.axelsson@stud.ki.se>
2018-03-13 10:50:25 +0100

    Now it works!

--- scripts/randomforest.py
+++ scripts/randomforest.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 #import data from np.savez 
 def vectors_savez(input_file):
+    global x, y
     filz = np.load(input_file)    
     x = filz['x'] #whatever name you have used to save the vectors 
     y = filz['y']
@@ -17,7 +18,7 @@ def vectors_savez(input_file):
 
 #use sklearn train_test_split to generate train test data split
 x, y = vectors_savez('../splitoutput/ws_35_alpha_beta_globular_sp_4state.txt_split_output.txt.npz')
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.10)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.30)
 
 #Training and testing the different classifyers LinearSVC, decisiontree and randomforest
 #SVM classifier
@@ -29,8 +30,9 @@ svm_cross_mean = svm_cross_score.mean()
 clf.fit(x_train, y_train)
 print('SVM training done...')
 svm_y_predicted = clf.predict(x_test)
-svm_classreport = classification_report(y_test, svm_y_predicted, labels = ['1', '2', '3', '4'])
-svm_confusionm = confusion_matrix(y_test, svm_y_predicted, labels = ['1', '2', '3', '4'])
+print(svm_y_predicted)
+svm_classreport = classification_report(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
+svm_confusionm = confusion_matrix(y_test, svm_y_predicted, labels = [1, 2, 3, 4])
 
 
 #decision tree classifier
@@ -42,8 +44,8 @@ tree_score_mean = tree_cross_score.mean()
 clf.fit(x_train, y_train)
 print('Decision tree training done...')
 tree_y_predicted = clf.predict(x_test)
-tree_classreport = classification_report(y_test, tree_y_predicted, labels = ['1', '2', '3', '4'])
-tree_confusionm = confusion_matrix(y_test, tree_y_predicted, labels = ['1', '2', '3', '4'])
+tree_classreport = classification_report(y_test, tree_y_predicted, labels = [1, 2, 3, 4])
+tree_confusionm = confusion_matrix(y_test, tree_y_predicted, llabels = [1, 2, 3, 4])
 
 
 #randomforestclassifier
@@ -54,9 +56,10 @@ print('Random forest cross validation done...')
 random_score_mean = random_cross_score.mean()
 clf.fit(x_train, y_train)
 print('Random forest training done...')
+
 random_y_predicted = clf.predict(x_test)
-random_classreport = classification_report(y_test, random_y_predicted, labels = ['1', '2', '3', '4'])
-random_confusionm = confusion_matrix(y_test, random_y_predicted, labels = ['1', '2', '3', '4'])
+random_classreport = classification_report(y_test, random_y_predicted, labels = [1, 2, 3, 4])
+random_confusionm = confusion_matrix(y_test, random_y_predicted, labels = [1, 2, 3, 4])
 
 
 with open ('../outputfiles/results_svm_tree_random.txt', 'w') as f:
