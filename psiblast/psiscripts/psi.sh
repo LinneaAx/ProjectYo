fcfeb860b558dd90603a47e5695b72da706ad648
LinneaAx <36500276+LinneaAx@users.noreply.github.com>
2018-03-16 11:40:05 +0100

    attempt to make it run for new sequences

--- psiblast/psiscripts/psi.sh
+++ psiblast/psiscripts/psi.sh
@@ -1,6 +1,13 @@
-cd ../fastas/
+cd ../fastas/predict/
 for file in *.fasta
 do 
 echo $file
-psiblast -query $file -evalue 0.001 -db /home/u2365/uniprot_sprot.fasta -num_iterations 3 -out_ascii_pssm /home/u2365/ProjectYo/psiblast/pssms/$file -num_threads=8
+psiblast -query $file -evalue 0.001 -db /home/u2365/uniprot_sprot.fasta -num_iterations 3 -out_ascii_pssm /home/u2365/ProjectYo/psiblast/pssms/to_predict/$file -num_threads=8
 done
+cd ../pssms/to_predict/
+for file in *.fasta
+python3 pssm_predict.py $file
+done
+
+
+
